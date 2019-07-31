de68ea586df93e4df66a931a026d62aae567a920
bagirovkenan <32015693+bagirovkenan@users.noreply.github.com>
2019-07-31 14:08:28 +0400

    Add files via upload

---
+++ MSSQL_Form-16.12.2018/MSSQL_Form-16.12.2018/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MSSQL_Form_16._12._2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 165);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Convertor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 288);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MSSQL_Form-16.12.2018/MSSQL_Form-16.12.2018/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace MSSQL_Form_16._12._2018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            var webClient = new WebClient();
+
+            var xmlDataByte = webClient.DownloadData("https://www.cbar.az/currencies/14.12.2018.xml");
+            var xmlData = Encoding.UTF8.GetString(xmlDataByte);
+
+
+            StringReader xmlStreamData = new StringReader(xmlData);
+
+            XmlSerializer xmlser = new XmlSerializer(typeof(ValCurs));
+            var myValCurs = (ValCurs)xmlser.Deserialize(xmlStreamData);
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+            SqlConnection coon = new SqlConnection(@"data source=192.168.19.35\SQLEXPRESS;User id=sa;Password=123456;Initial Catalog=p504");
+            coon.Open();
+
+            string comText = "";
+            var nDate = (myValCurs.Date).ToString();
+
+
+            //foreach (var i in myValCurs.ValType)
+            //  {
+            var ig = myValCurs.ValType[1];
+            var nValueType = ig.Type;
+
+            foreach (var y in ig.Valute)
+            {
+
+                var ncode = (y.Code).ToString();
+                var nNominal = (y.Nominal);
+                var nName = (y.Name).ToString();
+                var nValue = (y.Value);
+
+
+
+                comText += "Insert Into Currency(Code,Nominal,Name,Value,ValueType,ValDateTime,[User])" + $" VALUES('{ncode}',{nNominal}, N'{ nName}',{ nValue} ,'{ nValueType }','{ nDate }', 'kenan');";
+
+            }
+
+
+            //}
+            SqlCommand cm = new SqlCommand(comText, coon);
+            cm.ExecuteNonQuery();
+            coon.Close();
+
+
+
+        }
+    }
+
+    public class Valute
+    {
+        [XmlElement(ElementName = "Nominal")]
+        public string Nominal { get; set; }
+        [XmlElement(ElementName = "Name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "Value")]
+        public string Value { get; set; }
+        [XmlAttribute(AttributeName = "Code")]
+        public string Code { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ValType")]
+    public class ValType
+    {
+        [XmlElement(ElementName = "Valute")]
+        public List<Valute> Valute { get; set; }
+        [XmlAttribute(AttributeName = "Type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ValCurs")]
+    public class ValCurs
+    {
+        [XmlElement(ElementName = "ValType")]
+        public List<ValType> ValType { get; set; } = new List<ValType>();
+        [XmlAttribute(AttributeName = "Date")]
+        public string Date { get; set; }
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name { get; set; }
+        [XmlAttribute(AttributeName = "Description")]
+        public string Description { get; set; }
+    }
+
+
+}
